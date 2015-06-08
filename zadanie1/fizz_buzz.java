54c2fe8b5708396fe387e5e0f3857ed2c6f840f2
Kamil Best <kamil.besst@gmail.com>
2015-06-08 15:57:59 +0200

    ostateczna wersja

--- zadanie1/fizz_buzz.java
+++ zadanie1/fizz_buzz.java
@@ -1,32 +1,28 @@
 /**
  * Created by kamilbest on 08.06.15.
  */
-public class Zadanie1
-{
-    Zadanie1()
-    {
-        System.out.println("Zadanie1 - FizzBuzz");
+public class Zadanie1 {
+    public static void main(String[] args) {
+        FizzBuzz fb = new FizzBuzz();
+        fb.run();
+    }
+}
+
+class FizzBuzz {
+    private void runFizzBuzz() {
+        for (int i = 1; i <= 100; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0))
+                System.out.println("FizzBuzz");
+            else if (i % 3 == 0)
+                System.out.println("Fizz");
+            else if (i % 5 == 0)
+                System.out.println("Buzz");
+            else
+                System.out.println(i);
+        }
     }
 
-    class FizzBuzz
-    {
-        FizzBuzz()
-        {
-            for (int i = 1; i <= 100; i++) {
-                if ((i % 3 == 0) && (i % 5 == 0))
-                    System.out.println("FizzBuzz");
-                else if (i % 3 == 0)
-                    System.out.println("Fizz");
-                else if (i % 5 == 0)
-                    System.out.println("Buzz");
-                else
-                    System.out.println(i);
-            }        }
-    }
-
-    public static void main(String[] args)
-    {
-        Zadanie1 zadanie1 = new Zadanie1();
-        FizzBuzz fizzBuzz = zadanie1.new FizzBuzz();
+    public void run() {
+        runFizzBuzz();
     }
 }
