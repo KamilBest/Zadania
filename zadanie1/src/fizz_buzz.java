875c7b3e810994bf53ef89bf49a1d3c3d49ee128
Kamil Best <kamil.besst@gmail.com>
2015-06-10 12:42:40 +0200

    usuniecie zadanie1.txt, dodanie i przeniesienie pliku do src

---
+++ zadanie1/src/fizz_buzz.java
@@ -0,0 +1,32 @@
+public class Zadanie1 {
+    public static void main(String[] args) {
+        FizzBuzz fb = new FizzBuzz();
+        fb.run();
+    }
+}
+
+/**
+ * @author KamilBest
+ * @since 08.06.2015
+ * Program FizzBuzz wypisuje liczby od 1 do 100, ale dla wielokrotności trójki wyświetla "Fizz"
+zamiast liczby oraz dla wielokrotności piątki wyświetla "Buzz". Dla liczb będących wielokrotnościami
+trójki oraz piątki wyświetla "FizzBuzz".
+ */
+class FizzBuzz {
+    private void runFizzBuzz() {
+        for (int i = 1; i <= 100; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0))
+                System.out.println("FizzBuzz");
+            else if (i % 3 == 0)
+                System.out.println("Fizz");
+            else if (i % 5 == 0)
+                System.out.println("Buzz");
+            else
+                System.out.println(i);
+        }
+    }
+
+    public void run() {
+        runFizzBuzz();
+    }
+}
