03328f95bc093e8f464b27fce23f2631a6422c87
Kamil Best <kamil.besst@gmail.com>
2015-06-27 12:01:15 +0200

    dodanie testow, poprawa kodu

--- kod_kreskowy/src/main/java/KodKreskowy.java
+++ kod_kreskowy/src/main/java/KodKreskowy.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 /**
  * Klasa analizująca kody kreskowe EAN-13 lub EAN-8.
  * Wejściowy kod kreskowy: parametr tekstowy,
@@ -22,31 +23,29 @@ class KodKreskowy {
     /**
      * Metoda pobierajaca kod kreskowy od uzytkownika.
      */
-    private String pobierzKod() {
+    private void pobierzKod() {
         System.out.println("Podaj kod kreskowy: ");
         Scanner kod = new Scanner(System.in);
         kodKreskowy = kod.nextLine();
-        return kodKreskowy;
     }
 
     /**
      * Metoda pobierajaca rodzaj kodu kreskowego od uzytkownika.
      */
-    private int pobierzRodzajKodu()
-    {
+    private void pobierzRodzajKodu() {
         System.out.println("Podaj rodzaj kodu: (1-2):");
         Scanner rodzaj = new Scanner(System.in);
         rodzajKodu = rodzaj.nextInt();
-        return rodzajKodu;
     }
 
     /**
      * Metoda sprawdzajaca dlugosc podanego kodu kreskowego.
      *
-     * @return boolean, true jezeli dlugosc jest poprawna, w przeciwnym wypadku false.
+     * @param String podanyKod, int podanyRodzajKodu
+     * @return String, true jezeli dlugosc jest poprawna, w przeciwnym wypadku false.
      * @throws IllegalArgumentException, wyrzucenie wyjatku w przypadkach: nieprawidlowy rodzaj lub dlugosc kodu.
      */
-    public boolean sprawdzDlugoscKodu(String podanyKod, int podanyRodzajKodu){
+    public String sprawdzDlugoscKodu(String podanyKod, int podanyRodzajKodu) {
         dlugoscKodu = podanyKod.length();
 
          /*
@@ -55,67 +54,82 @@ class KodKreskowy {
         rodzaj kodu=1, 8, 10 lub 13
         rodzaj kodu=2, 13, 15 lub 18
          */
-        if (( podanyRodzajKodu < 1) || ( podanyRodzajKodu > 2))
+        if ((podanyRodzajKodu != 1) && (podanyRodzajKodu != 2))
             throw new IllegalArgumentException("Nieprawidlowy rodzaj kodu! Wymagany rodzaj 1 lub 2.");
-        else if (( podanyRodzajKodu == 1) && (dlugoscKodu < 8))
+        else if ((podanyRodzajKodu == 1) && (dlugoscKodu < 8))
             throw new IllegalArgumentException("Nieprawidlowa dlugosc kodu dla rodzaju 1 (EAN-8)!");
-        else if (( podanyRodzajKodu == 2) && (dlugoscKodu < 13))
+        else if ((podanyRodzajKodu == 2) && (dlugoscKodu < 13))
             throw new IllegalArgumentException("Nieprawidlowa dlugosc kodu dla rodzaju 2 (EAN-13)!");
-        else if (( podanyRodzajKodu == 1) && ((dlugoscKodu == 8) || (dlugoscKodu == 10) || (dlugoscKodu == 13)))
-            return true;
-        else if (( podanyRodzajKodu == 2)
-                && ((dlugoscKodu == 13) || (dlugoscKodu == 15) || (dlugoscKodu == 18)))
-            return true;
+        else if ((podanyRodzajKodu == 1) && ((dlugoscKodu != 8) && (dlugoscKodu != 10) && (dlugoscKodu != 13)))
+            throw new IllegalArgumentException("Nieprawidlowa dlugosc kodu dla rodzaju 1 (EAN-8)!");
+        else if ((podanyRodzajKodu == 2)
+                && ((dlugoscKodu != 13) && (dlugoscKodu != 15) && (dlugoscKodu != 18)))
+            throw new IllegalArgumentException("Nieprawidlowa dlugosc kodu dla rodzaju 2 (EAN-13)!");
         else
-            return false;
+            return podanyKod;
     }
 
     /**
      * Metoda sprawdzajaca dane wejsciowe.
      * Obsluga przypadku uciecia wiodacego zera przez skaner.
      *
+     * @param String
      * @return boolean true jezeli dane poprawne, false jezeli nie
      */
-    private boolean sprawdzDaneWejsciowe(){
-        kodDoAnalizy = kodKreskowy.toCharArray();
+    public boolean sprawdzUciecieZera(String podanyKod) {
+        dlugoscKodu = podanyKod.length();
 
            /*
         Sprawdzenie, czy skaner nie wycial z podanego kodu kreskowego wiodacego zera.
          */
-        char pierwszyZnak = kodKreskowy.charAt(0);
+        char pierwszyZnak = podanyKod.charAt(0);
 
         if ((dlugoscKodu == 7 && pierwszyZnak == '0') || (dlugoscKodu == 12 && pierwszyZnak == '0')) {
-            kodKreskowy = "0" + kodKreskowy;
-            kodDoAnalizy = kodKreskowy.toCharArray();
             return true;
-        } else if(sprawdzDlugoscKodu(kodKreskowy,rodzajKodu))
-            return true;
+        }
         return false;
+    }
+
+    /**
+     * Dodaje wiodace zero jezeli nastapilo uciecie.
+     *
+     * @param String podanyKod
+     * @return String
+     */
+    private String dodajWiodaceZero(String podanyKod) {
+        podanyKod = "0" + podanyKod;
+        kodDoAnalizy = podanyKod.toCharArray();
+        return podanyKod;
     }
 
     /**
      * Metoda analizujaca kod kreskowy.
      * Jezeli dane sa prawidlowe, kod zapisywany jest bez addonu do tablicy 8 lub 13 znakowej.
      *
+     * @param String podanyKod, int podanyRodzajKodu
      * @return String zwraca prawidlowy kod kreskowy bez addonu
      */
-    private String analizujKod(){
+    public String zwrocPoprawnyKod(String podanyKod, int podanyRodzajKodu) {
+        if (sprawdzUciecieZera(podanyKod) == true)
+            podanyKod = dodajWiodaceZero(podanyKod);
+        else
+            kodDoAnalizy = podanyKod.toCharArray();
+        sprawdzDlugoscKodu(podanyKod, podanyRodzajKodu);
         char[] poprawnyKodKreskowy = new char[13];
         String poprawnyKod = "";
-        if (sprawdzDaneWejsciowe()) {
-            System.out.println("Rodzaj kodu i kod_kreskowy są poprawne. ");
-            System.out.println("Prawidlowy kod kreskowy bez addonu:");
-            if (rodzajKodu == 1)
-                for (int i = 0; i < 8; i++) {
-                    poprawnyKodKreskowy[i] = kodDoAnalizy[i];
-                    poprawnyKod += poprawnyKodKreskowy[i];
-                }
-            else if (rodzajKodu == 2)
-                for (int i = 0; i < 13; i++) {
-                    poprawnyKodKreskowy[i] = kodDoAnalizy[i];
-                    poprawnyKod += poprawnyKodKreskowy[i];
-                }
-        }
+        System.out.println("Rodzaj kodu i kod_kreskowy są poprawne. ");
+        System.out.println("Prawidlowy kod kreskowy bez addonu:");
+        if (podanyRodzajKodu == 1)
+            for (int i = 0; i < 8; i++) {
+                poprawnyKodKreskowy[i] = kodDoAnalizy[i];
+                poprawnyKod += poprawnyKodKreskowy[i];
+            }
+        else if (podanyRodzajKodu == 2)
+            for (int i = 0; i < 13; i++) {
+                poprawnyKodKreskowy[i] = kodDoAnalizy[i];
+                poprawnyKod += poprawnyKodKreskowy[i];
+            }
+        System.out.println(poprawnyKod);
         return poprawnyKod;
     }
 
@@ -123,8 +137,8 @@ class KodKreskowy {
      * Metoda uruchamiajaca sprawdzenie poprawnosci danych wejsciowych i analize kodu kreskowego.
      */
     public void uruchom() {
-        if (sprawdzDaneWejsciowe() == false)
-            System.out.println("Niepoprawne dane!");
-        System.out.println(analizujKod());
+        pobierzKod();
+        pobierzRodzajKodu();
+        System.out.println(zwrocPoprawnyKod(kodKreskowy, rodzajKodu));
     }
 }
--- kod_kreskowy/src/test/java/KodKreskowyTest.java
+++ kod_kreskowy/src/test/java/KodKreskowyTest.java
@@ -11,10 +11,39 @@ public class KodKreskowyTest {
     public void setUp() {
         kodKreskowy = new KodKreskowy();
     }
-    @Test
-    public void sprawdzPoprawnoscDlugosciKodu()
-    {
-        assertTrue(kodKreskowy.sprawdzDlugoscKodu("12345678", 1));
 
+    @Test
+    public void sprawdzPoprawnoscDlugosciKodu() {
+        assertEquals("12345678", kodKreskowy.sprawdzDlugoscKodu("12345678", 1));
+        assertEquals("96385074", kodKreskowy.sprawdzDlugoscKodu("96385074", 1));
+        assertEquals("1234567890", kodKreskowy.sprawdzDlugoscKodu("1234567890", 1));
+        assertEquals("1234567890123", kodKreskowy.sprawdzDlugoscKodu("1234567890123", 1));
+
+        assertEquals("5901234123457", kodKreskowy.sprawdzDlugoscKodu("5901234123457", 2));
+        assertEquals("123456789012345", kodKreskowy.sprawdzDlugoscKodu("123456789012345", 2));
+        assertEquals("123456789012345678", kodKreskowy.sprawdzDlugoscKodu("123456789012345678", 2));
+    }
+
+    @Test
+    public void sprawdzCzyUcieloZero() {
+        assertTrue(kodKreskowy.sprawdzUciecieZera("075678164125"));
+        assertTrue(kodKreskowy.sprawdzUciecieZera("007567816412"));
+        assertTrue(kodKreskowy.sprawdzUciecieZera("000678164125"));
+        assertTrue(kodKreskowy.sprawdzUciecieZera("0123456"));
+        assertTrue(kodKreskowy.sprawdzUciecieZera("0000000"));
+    }
+
+    @Test
+    public void sprawdzPoprawnoscKodu() {
+        assertEquals("12345678", kodKreskowy.zwrocPoprawnyKod("12345678", 1));
+        assertEquals("12345678", kodKreskowy.zwrocPoprawnyKod("1234567893", 1));
+        assertEquals("11111111", kodKreskowy.zwrocPoprawnyKod("1111111189345", 1));
+
+        assertEquals("1234567890123", kodKreskowy.zwrocPoprawnyKod("1234567890123", 2));
+        assertEquals("1234567890123", kodKreskowy.zwrocPoprawnyKod("123456789012399", 2));
+        assertEquals("1234567890123", kodKreskowy.zwrocPoprawnyKod("123456789012399999", 2));
+
+        assertEquals("0075678164125", kodKreskowy.zwrocPoprawnyKod("075678164125", 2));
+        assertEquals("00123456", kodKreskowy.zwrocPoprawnyKod("0123456", 1));
     }
 }
